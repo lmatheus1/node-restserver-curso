9911a85bd8b7e842e3a2a0d6566763c03e43f08f
Ludwig <lmatheus1@gmail.com>
2020-12-29 10:00:29 -0400

    Login normal seccion 10

--- server/config/config.js
+++ server/config/config.js
@@ -11,6 +11,21 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // ============================
+//              Vencimiento del token
+//=============================
+//60 segundos
+//60 minutos
+//24 horas
+//30 dias
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 60;
+
+
+// ============================
+//              SEED de autenticación
+//=============================
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
+
+// ============================
 //              Base de Datos
 //=============================
 
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -3,17 +3,17 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const _ = require('underscore');
 const Usuario = require('../models/usuario');
+const { verificaToken, verificaAdmin_Role } = require('../middlewares/autenticacion');
+
 
 const app = express();
 
-
-app.get('/usuario', function(req, res) {
-
+app.get('/usuario', verificaToken, (req, res) => {
     let desde = req.query.desde || 0;
     desde = Number(desde);
-
     let limite = req.query.limite || 5;
     limite = Number(limite);
+
 
     Usuario.find({ estado: true }, 'nombre email role estado google img')
         .skip(desde)
@@ -26,20 +26,17 @@ app.get('/usuario', function(req, res) {
                 });
             }
 
-            Usuario.count({ estado: true }, (err, conteo) => {
+            Usuario.count({ estado: true }, (_err, conteo) => {
                 res.json({
                     ok: true,
                     usuarios,
                     cuantos: conteo
                 })
             });
-
-
         })
-
 });
 
-app.post('/usuario', function(req, res) {
+app.post('/usuario', [verificaToken, verificaAdmin_Role], function(req, res) {
 
     let body = req.body;
 
@@ -66,13 +63,10 @@ app.post('/usuario', function(req, res) {
 
 });
 
-app.put('/usuario/:id', function(req, res) {
+app.put('/usuario/:id', [verificaToken, verificaAdmin_Role], function(req, res) {
 
     let id = req.params.id;
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
-
-
-
 
     Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
 
@@ -92,16 +86,16 @@ app.put('/usuario/:id', function(req, res) {
 
 });
 
-app.delete('/usuario/:id', function(req, res) {
+app.delete('/usuario/:id', [verificaToken, verificaAdmin_Role], function(req, res) {
 
     let id = req.params.id;
-    //Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+    Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
 
-    let cambiaEstado = {
-        estado: false
-    };
+        // let cambiaEstado = {
+        //     estado: false
+        // };
 
-    Usuario.findByIdAndUpdate(id, cambiaEstado, { new: true }, (err, usuarioBorrado) => {
+        // Usuario.findByIdAndUpdate(id, cambiaEstado, { new: true }, (err, usuarioBorrado) => {
         if (err) {
             return res.status(400).json({
                 ok: false,
